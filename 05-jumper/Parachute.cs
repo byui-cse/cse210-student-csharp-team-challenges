51b1a3efaeac1dbf2ddde02566e80cc087bd50bb
Benbob02 <91492582+Benbob02@users.noreply.github.com>
2021-10-11 15:35:00 -0600

    Add files

---
+++ 05-jumper/Parachute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _05_jumper
+{
+    class Parachute
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting point for the Jumper project.");
+        }
+    }
+}
