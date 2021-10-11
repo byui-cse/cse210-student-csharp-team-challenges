871d5d3bc0f16ada6771b7084f601fdcbbbd7445
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2021-10-11 15:56:08 -0600

    Add code to UserService

---
+++ 05-jumper/UserService.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _05_jumper
+{
+    public class UserService
+    {
+        static void DisplayText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static void getLetterInput(string promptText)
+        {
+            Console.Write(promptText);
+            string letter = Console.ReadLine();
+
+        } 
+    }
+}
