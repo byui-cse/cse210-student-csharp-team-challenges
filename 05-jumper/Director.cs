b8dcb336f716a4773e50f27719efe18369cc47ea
Benbob02 <91492582+Benbob02@users.noreply.github.com>
2021-10-11 15:56:15 -0600

    Add variables to directors

--- 05-jumper/Director.cs
+++ 05-jumper/Director.cs
@@ -4,6 +4,9 @@ namespace _05_jumper
 {
     class Director
     {
+        public bool _keepPlaying;
+        public Parachute _parachute;
+        public UserService _userService;
         static void Main(string[] args)
         {
             Console.WriteLine("Starting point for the Jumper project.");
