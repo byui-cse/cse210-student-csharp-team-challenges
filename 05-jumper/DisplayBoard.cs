9150936258fd07f1fe2440fd1c2874ef0b87bc8d
SpaceDadNewman <91348837+SpaceDadNewman@users.noreply.github.com>
2021-10-13 12:04:28 -0600

    add displayboard

---
+++ 05-jumper/DisplayBoard.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _05_jumper
+{
+    public class Board
+    {
+        static void DisplayBoard()
+        {
+            Console.WriteLine("Starting point for the Jumper project.");
+        }
+    }
+}
