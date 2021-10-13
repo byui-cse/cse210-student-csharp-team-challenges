fc90fc895ef5e1ca567541994dbac225d0433106
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2021-10-13 11:15:47 -0600

    Update UserService.cs

--- 05-jumper/UserService.cs
+++ 05-jumper/UserService.cs
@@ -1,12 +1,1 @@
-using System;
 
-namespace _05_jumper
-{
-    class UserService
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Starting point for the Jumper project.");
-        }
-    }
-}
