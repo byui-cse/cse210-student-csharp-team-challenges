0d8db5c78b27296cdfaea75d52f496cc5cc7eee6
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2021-10-13 11:48:17 -0600

    Added member variable _keepPlaying

--- 05-jumper/Director.cs
+++ 05-jumper/Director.cs
@@ -4,6 +4,7 @@ namespace _05_jumper
 {
     public class Director
     {
+        public bool _keepPlaying = true;
         public void StartGame()
         {
             Console.WriteLine("Starting point for the Jumper project.");
