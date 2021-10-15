a147f13e54f1f5dbcfa77d2eec65e5642e0d1919
LindseyK2 <lindseyk2@byui.edu>
2021-10-15 12:59:37 -0600

    Just testing

--- 05-jumper/workBank.cs
+++ 05-jumper/workBank.cs
@@ -4,9 +4,11 @@ namespace _05_jumper
 {
     public class WordBank
     {
-        static void Main(string[] args)
+        public string _word;
+        static string getWord()
         {
-            Console.WriteLine("Starting point for the Jumper project.");
+           string _word = "Taco";
+           return _word;
         }
     }
 }
