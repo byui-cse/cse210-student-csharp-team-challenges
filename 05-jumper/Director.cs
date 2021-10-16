67b2032b490b5568344ad9106fc217024d5f8420
ElGranGerezim <nazanal@live.com>
2021-10-15 18:45:02 -0600

    Began work on director.

--- 05-jumper/Director.cs
+++ 05-jumper/Director.cs
@@ -2,36 +2,74 @@ using System;
 
 namespace _05_jumper
 {
+    /// <Summary>
+    /// Main class of program, manages game loop.
+    /// </Summary>
     class Director
     {
-        public void StartGame()
-        {
+        UserService _userService = new UserService();
+        Jumper _jumper = new Jumper();
+        Word _word = new Word();
+        bool _playing = true;
 
-        }
-
-        public void PlayGame()
-        {
-
-        }
-
-        public void GetInputs()
-        {
-
-        }
-
-        public void DoUpdates()
-        {
-
-        }
-
-        public void DoOutput()
-        {
-
-        }
-
-        public void EndGame()
+        /// <Summary>
+        /// Initializes the game.
+        /// </Summary>
+        private void StartGame()
         {
             
+        }
+
+        /// <Summary>
+        /// Monitors main gameplay loop.
+        /// </Summary>
+        public void PlayGame()
+        {
+            while(_playing){
+                DoOutput();
+                char guess = GetGameInput();
+                DoUpdates(guess);
+            }
+            EndGame();
+        }
+        
+        /// <Summary>
+        /// Input processor for main gameplay loop
+        /// </Summary>
+        private char GetGameInput()
+        {
+            char guess =_userService.GetGuess();
+            return guess;
+        }
+
+        /// <Summary>
+        /// Updates gameplay state based on user input
+        /// </Summary>
+        private void DoUpdates(char guess)
+        {
+            if (!_jumper.IsAlive() || _word.isGuessed()){
+                _playing = false;
+            } else if (!_word.CheckGuess(guess)){
+                _jumper.LoseLife();
+            }
+        }
+
+        /// <Summary>
+        /// Displays game state to user
+        /// </Summary>
+        private void DoOutput()
+        {
+            string wordState = _word.GetWordDisplay();
+            string jumperState = _jumper.GetStatus();
+            _userService.DisplayBoard(wordState,jumperState);
+        }
+
+        /// <Summary>
+        /// Displays end of game output prompting to play again
+        /// </Summary>
+        public bool EndGame()
+        {
+            return _userService.GetPlayAgain();
         }
     }
 }
