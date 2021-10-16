d566bcc41f0d0117a7df5ca3abf7880f6e1ff88a
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2021-10-16 14:59:32 -0600

    Meh

--- 05-jumper/WordBank.cs
+++ 05-jumper/WordBank.cs
@@ -5,12 +5,12 @@ namespace _05_jumper
 {
     public class WordBank
     {
-        public List<string> _bank = new List<string>();
+        public Random rand = new Random();
         
         public string GetWord()
         {
-            _bank.Add("Funkytown");
-            return _bank[0];
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\dtytr\OneDrive\Documents\GitHub\cse210-student-csharp-team-challenges\05-jumper\WordList");
+            return lines[rand.Next(0,lines.Length - 1)];
         }
     }
 }
