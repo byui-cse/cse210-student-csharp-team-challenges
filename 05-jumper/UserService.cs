b783f1647b23608dae6233991b016a90251011c0
faraguti <faraguti@gmail.com>
2021-10-15 19:50:29 -0600

    User Service - test1

--- 05-jumper/UserService.cs
+++ 05-jumper/UserService.cs
@@ -2,26 +2,83 @@ using System;
 
 namespace _05_jumper
 {
+    /// <Summary>
+    /// 
+    /// </Summary>
     class UserService
     {
+
+        /// <Summary>
+        /// Asks the user if he wants to play again. Validates the input. Returns answer.
+        /// </Summary>
         public bool GetPlayAgain()
         {
+            while(true){
+                Console.WriteLine("Do you want to play again?(Y / N): ");
+            
+                string input = Console.ReadLine();
 
+                if (input.Length == 1)
+                {
+                    if (input == "Y")
+                    {
+                        return true;
+                    }
+                    if (input == "N")
+                    {
+                        return false;
+                    }
+                }
+
+                Console.WriteLine("Please choose Y or N");
+            }  
         }
-
+        /// <Summary>
+        /// 
+        /// </Summary>
         public void DisplayBoard(string word, string jumper)
         {
 
         }
 
+        /// <Summary>
+        /// Asks the user for a letter. Validates input. Returns input (if valid).
+        /// </Summary>
         public char GetGuess()
         {
+            while(true){
+                Console.WriteLine("Choose one letter: ");
+            
+                string input = Console.ReadLine();
 
+                if (input.Length == 1)
+                {
+                    char checkChar = char.Parse(input);
+
+                    if (char.IsLetter(checkChar))
+                    {
+                        return checkChar;
+                    }
+                }
+
+                Console.WriteLine("Please choose just ONE letter.");
+            }  
         }
 
-        private string FormatWord()
+        /// <Summary>
+        /// 
+        /// </Summary>
+        private string FormatWord(string baseWord)
         {
-            
+            // computer
+            string output = "";
+
+            foreach (char letter in baseWord){
+                output += letter + " ";
+                
+            }
+
+            return output;
         }
     }
 }
