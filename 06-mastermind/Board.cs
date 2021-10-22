199e85633688dd6049b97462f202bf9e06093379
SpaceDadNewman <91348837+SpaceDadNewman@users.noreply.github.com>
2021-10-22 15:07:36 -0600

    board

--- 06-mastermind/Board.cs
+++ 06-mastermind/Board.cs
@@ -5,11 +5,53 @@ namespace _06_mastermind
 {
     class Board
     {
-        private int code;
+        private string _number;
         public Board()
         {
-            Random rng = new Random();
-            code= rng.Next(1000,10000);
+            Random rnd = new Random();
+            _number += rnd.Next(1000,10000);
+        }
+        public string Display(string name, string guess)
+        {
+            string DisplayText = "";
+            DisplayText += $"Player {name}: {guess}, ";
+            DisplayText += GetHint(guess);
+
+            return DisplayText;
+        }
+
+        private string GetHint(string guess)
+        {
+            string HintText = "";
+            for (int i = 0; i < 4; i++)
+            {
+                if (guess[i] == _number[i])
+                {
+                    HintText += "x";
+                }
+                else if (_number.Contains(guess[i]))
+                {
+                    HintText += "o";
+                }
+                else
+                {
+                    HintText += "*";
+                }
+            }
+
+            return HintText;
+        }
+
+        private bool CheckWin(string guess)
+        {
+            if (guess == _number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
