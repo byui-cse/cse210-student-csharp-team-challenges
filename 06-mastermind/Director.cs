95807e37da79da639bbd1fc2241fe995092254e4
Benbob02 <91492582+Benbob02@users.noreply.github.com>
2021-10-22 15:26:21 -0600

    director

--- 06-mastermind/Director.cs
+++ 06-mastermind/Director.cs
@@ -10,7 +10,11 @@ namespace _06_mastermind
         }
         public void StartGame()
         {
-
+            UserService _userservice = new UserService();
+            string name = _userservice.GetStringInput("What is the name of player 1?  ");
+            Player _player1 = new Player(name);
+            name = _userservice.GetStringInput("What is the name of player 2?  ");
+            Player _player2 = new Player(name);
         }
     }
 }
