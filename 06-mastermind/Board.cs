5ba3a3402b42ad10771978f8fe6a5838263e5821
ElGranGerezim <nazanal@live.com>
2021-10-23 16:47:54 -0600

    Comments added

--- 06-mastermind/Board.cs
+++ 06-mastermind/Board.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace _06_mastermind
 {
+    /// <summary>
+    /// The class that takes care of the current state of the players.
+    /// </summary>
     class Board
     {
         private Player[] _players;
@@ -11,6 +14,9 @@ namespace _06_mastermind
 
         private int NUM_PLAYERS = 2;
 
+        /// <summary>
+        /// Constructor to generate new player objects on startup.
+        /// </summary>
         public Board(){
             _players = new Player[NUM_PLAYERS];
             for (int i = 0; i < NUM_PLAYERS; i++){
@@ -18,7 +24,11 @@ namespace _06_mastermind
             }
         }
 
-        public string formatTurnDisplay()
+        /// <summary>
+        /// Creates a string containing the display of the current turn.
+        /// </summary>
+        /// </param> win: whether or not to include the active player as the winner.
+        public string formatTurnDisplay(bool win = false)
         {
             string turnDisplay = "---------------------\n";
             foreach (Player person in _players){
@@ -26,15 +36,26 @@ namespace _06_mastermind
                 turnDisplay += "\n";
             }
             turnDisplay += "---------------------\n";
+            turnDisplay += $"{_activePlayer} Wins!";
             return turnDisplay;
         }
-        private void updatePlayer(string guess, string hint)
+
+        /// <summary>
+        /// Stores the current moves for the turn in the active player, then switches active player.
+        /// </summary>
+        /// </params> guess: what the player guessed
+        ///         hint: what hint the player was given
+        public void updatePlayer(string guess, string hint)
         {
             _players[_activePlayer].setLastGuess(guess);
             _players[_activePlayer].setLastHint(hint);
             switchActivePlayer();
         }
 
+        
+        /// <summary>
+        /// Switches the active player to the next in the list. If at end of list, returns to start of list.
+        /// </summary>
         private void switchActivePlayer(){
             if (_activePlayer == _players.Length - 1){
                 _activePlayer = 0;
@@ -43,10 +64,14 @@ namespace _06_mastermind
             }
         }
 
+        /// <summary>
+        /// Sets the name of each player.
+        /// </summary>
         public void namePlayers(List<string> playerNames){
             for (int i = 0; i < playerNames.Count; i++){
                 _players[i].setName(playerNames[i]);
             }
+            List<string> bruh = new List<string>{};
         }
 
     }
--- 06-mastermind/Player.cs
+++ 06-mastermind/Player.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace _06_mastermind
 {
+    /// <summary>
+    /// Class that basically acts as a struct to hold data about each player
+    /// </summary>
     class Player
     {
         string _lastGuess = "----";
@@ -10,19 +13,36 @@ namespace _06_mastermind
 
         string _name;
 
+        /// <summary>
+        /// Sets the player name
+        /// </summary>
+        /// </params> name: what to set the name as
         public void setName(string name){
             _name = name;
         }
 
+        /// <summary>
+        /// Returns the player name
+        /// </summary>
+        /// <returns> _name
         public void setLastGuess(string guess)
         {
             _lastGuess = guess;
         }
 
+        /// <summary>
+        /// Stores the hint in _lastHint
+        /// </summary>
+        /// </params> hint: what to save in _lastHint
         public void setLastHint(string hint)
         {
             _lastHint = hint;
         }
+
+        /// <summary>
+        /// Returns this players portion of the turn display
+        /// </summary>
+        /// </returns> A string formatted for use in turn display.
         public string getTurnDisplay()
         {
             string turnDisplay = $"Player {_name}: {_lastGuess}, {_lastHint}";
