24a121e67083241178b57d7ffeca71624931e368
ElGranGerezim <nazanal@live.com>
2021-11-01 12:03:36 -0600

    Enemies first draft, import wordlist

--- 07-speed/Enemies.cs
+++ 07-speed/Enemies.cs
@@ -1,22 +1,40 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace _07_speed
 {
     class Enemies : Actor
     {
+        private int _points;
 
-        public Enemies()
+        /// <summary> Constructor to generate new enemy. 
+        /// <params> Speed: how fast the enemy moves
+        /// <params> Height: How high on the screen to spawn enemy
+        public Enemies(int speed, int height)
         {
-
+            _position = new Point(Constants.MAX_X, height);
+            SetVelocity(new Point(speed, 0));
+            SelectWord();
+            _points = _text.Length;
         }
 
-        int _points;
-
-        int _speed;
-
-        public void move()
+        /// <summary> Selects a word at random from a txt file and changes the _text member variable.
+        public void SelectWord()
         {
-            
+            List<string> lines = new List<string>(File.ReadAllLines("WordList.txt"));
+            Random r = new Random();
+            int randomLineNumber = r.Next(0, lines.Count - 1);
+            string word = lines[randomLineNumber];
+
+            _text = word;
+        }
+
+        /// <summary> Returns the point value of the enemy
+        /// <returns> Int: _points
+        public int getPoint(){
+            return _points;
         }
 
     }
